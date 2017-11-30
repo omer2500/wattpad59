458ce2fbaebe3c9ba9926a6b193c60f02bf38926
omer2500 <omer2500@gmail.com>
2017-11-30 16:42:42 +0200

    fixed navbar

--- app/src/main/java/com/example/omer/wattpad59/AddStoryInfoActivity.java
+++ app/src/main/java/com/example/omer/wattpad59/AddStoryInfoActivity.java
@@ -1,8 +1,11 @@
 package com.example.omer.wattpad59;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 /**
@@ -19,6 +22,33 @@ public class AddStoryInfoActivity extends AppCompatActivity {
         TextView title = (TextView) findViewById(R.id.activityTitleAddStoryInfo);
         title.setText("This is activity add story info");
 
+        //***************************BOTTOM NAVIGATION BAR*****************************************************
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottomNavView_Bar);
+        BottomNavigationViewHelper.removeShiftMode(bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.ic_home:
+                        Intent intent = new Intent(AddStoryInfoActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        break;
+
+                    case R.id.ic_library:
+                        Intent intent1 = new Intent(AddStoryInfoActivity.this, LibraryActivity.class);
+                        startActivity(intent1);
+                        break;
+
+                    case R.id.ic_add_story_info:
+                        Intent intent2 = new Intent(AddStoryInfoActivity.this, AddStoryInfoActivity.class);
+                        startActivity(intent2);
+                        break;
+                }
+                return false;
+            }
+        });
+        //***************************BOTTOM NAVIGATION BAR*****************************************************
 
     }
 }
--- app/src/main/java/com/example/omer/wattpad59/LibraryActivity.java
+++ app/src/main/java/com/example/omer/wattpad59/LibraryActivity.java
@@ -20,5 +20,33 @@ public class LibraryActivity extends AppCompatActivity {
         TextView title = (TextView) findViewById(R.id.activityTitleLibrary);
         title.setText("This is activity library");
 
+        //***************************BOTTOM NAVIGATION BAR*****************************************************
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottomNavView_Bar);
+        BottomNavigationViewHelper.removeShiftMode(bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.ic_home:
+                        Intent intent = new Intent(LibraryActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        break;
+
+                    case R.id.ic_library:
+                        Intent intent1 = new Intent(LibraryActivity.this, LibraryActivity.class);
+                        startActivity(intent1);
+                        break;
+
+                    case R.id.ic_add_story_info:
+                        Intent intent2 = new Intent(LibraryActivity.this, AddStoryInfoActivity.class);
+                        startActivity(intent2);
+                        break;
+                }
+                return false;
+            }
+        });
+        //***************************BOTTOM NAVIGATION BAR*****************************************************
+
     }
 }
